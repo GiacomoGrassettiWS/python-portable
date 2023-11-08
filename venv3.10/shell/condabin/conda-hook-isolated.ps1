2cb12d9d2f3ddaf7236f7a66467bef365987604d
Giacomo Grassetti <giacomo.grassetti@websolute.it>
2023-11-08 13:06:38 +0100

    adde venv

---
+++ venv3.10/shell/condabin/conda-hook-isolated.ps1
@@ -0,0 +1,17 @@
+$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+$Env:_CONDA_ROOT = "$scriptPath/../.."
+$Env:CONDA_EXE = "$Env:_CONDA_ROOT/condabin/conda"
+$Env:_CE_M = ""
+$Env:_CE_CONDA = ""
+$Env:_CONDA_EXE = "$Env:_CONDA_ROOT/condabin/conda"
+
+# Fix PATH - stupid, but works
+$Env:PATH = "$Env:_CONDA_ROOT;$Env:_CONDA_ROOT/Scripts;$Env:_CONDA_ROOT/Library/mingw-w64/bin;$Env:_CONDA_ROOT/Library/usr/bin;$Env:_CONDA_ROOT/Library/bin;$Env:_CONDA_ROOT/bin;$Env:_CONDA_ROOT/condabin;"
+
+# We have omitted %PATH% at the end. We want fully isolated Win PATH if possible.
+# E.g. Intel compiler (tools) on path interfere /w numba precompilation of sun_geometry causing SDAT to crash.
+
+$CondaModuleArgs = @{ChangePs1 = $True}
+Import-Module "$Env:_CONDA_ROOT/shell/condabin/Conda.psm1" -ArgumentList $CondaModuleArgs
+Remove-Variable CondaModuleArgs
+Add-CondaEnvironmentToPrompt
